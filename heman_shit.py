611cd482b6e32eebcbd18462f71612930ce3b36a
Heman Gandhi <hemangandhi@gmail.com>
2019-04-18 14:56:07 -0400

    get max of a bunch of runs

--- heman_shit.py
+++ heman_shit.py
@@ -12,7 +12,7 @@ def make_sine(period_in_dt=21, dt_test=.0001):
     arr = (10 * np.sin(times) + 1000) * Hz
     return arr
 
-def make_snn_and_run(ts, aggregator, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second, runs=1):
+def make_snn_and_run_once(ts, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second):
     # constants, equations, detritus
     ts = TimedArray(ts, dt=dt_ts)
     N = 1000
@@ -74,14 +74,14 @@ def make_snn_and_run(ts, aggregator, lags=[2, 3, 5], duration=1*second, dt_ts=0.
     # mon = StateMonitor(neurons, variables = ['v'],record=range(10000), dt=0.0001 * second )
 
     # Run and record
-    for r in range(runs):
-        run(duration, report='text')
-        aggregator(r, sss)
-        #TODO: this is a resource issue otherwise?
-        #also, what do >1 second runs mean?
-        #and how reset SNN?
-        sss.resize(0)
-        print("run {} done".format(r))
+    run(duration, report='text')
+    return sss
+
+def run_many_times(ts, aggregator, runs, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second):
+    for i in range(runs):
+        sss = make_snn_and_run_once(ts, lags, duration, dt_ts)
+        aggregator(i, sss)
+        print("Run {} done".format(i))
 
 def print_statzi(nv):
     def aggie(i, sss):
@@ -95,9 +95,19 @@ def plot_statzi(lags):
         plt.show()
     return aggie
 
+def lag_is_max(lags):
+    lgd = {i: 0 for i in lags}
+    def aggie(i, sss):
+        it = -1
+        for it in zip(*sss.w[0:len(lags)]):
+            pass
+        ml = lags[max(range(len(lags)), key=lambda x: it[x])]
+        lgd[ml] += 1
+    return aggie, lgd
+
 if __name__ == "__main__":
-    # plot_statzi(make_snn_and_run(make_x()), 3)
-    # plot_statzi(make_snn_and_run(make_sine()), 3)
     x_plus_sin = make_x(x_scale=5) + make_sine()
-    guy_fawkes = plot_statzi([2, 3, 5])
-    make_snn_and_run(x_plus_sin, guy_fawkes, runs=3)
+    # guy_fawkes = plot_statzi([2, 3, 5])
+    maxer, di = lag_is_max([2, 3, 5])
+    run_many_times(x_plus_sin, maxer, 3)
+    print(di)
