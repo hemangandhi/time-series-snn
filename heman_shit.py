6eea488f19a7115ae1c09f7bba30f873ace009d6
Heman Gandhi <hemangandhi@gmail.com>
2019-04-18 14:39:32 -0400

    multiple runs handled

--- heman_shit.py
+++ heman_shit.py
@@ -12,7 +12,7 @@ def make_sine(period_in_dt=21, dt_test=.0001):
     arr = (10 * np.sin(times) + 1000) * Hz
     return arr
 
-def make_snn_and_run(ts, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second):
+def make_snn_and_run(ts, aggregator, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second, runs=1):
     # constants, equations, detritus
     ts = TimedArray(ts, dt=dt_ts)
     N = 1000
@@ -74,19 +74,30 @@ def make_snn_and_run(ts, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * secon
     # mon = StateMonitor(neurons, variables = ['v'],record=range(10000), dt=0.0001 * second )
 
     # Run and record
-    run(duration, report='text')
-    return sss
+    for r in range(runs):
+        run(duration, report='text')
+        aggregator(r, sss)
+        #TODO: this is a resource issue otherwise?
+        #also, what do >1 second runs mean?
+        #and how reset SNN?
+        sss.resize(0)
+        print("run {} done".format(r))
 
-def print_statzi(sss, nv):
-    list(map(print,zip(*sss.w[0:nv])))
+def print_statzi(nv):
+    def aggie(i, sss):
+        list(map(print,zip(*sss.w[0:nv])))
+    return aggie
 
-def plot_statzi(sss, lags):
-    list(map(lambda x: plt.plot(x[0], label=x[1]),zip(sss.w[0:len(lags)], ("t - {}".format(l) for l in lags))))
-    plt.legend()
-    plt.show()
+def plot_statzi(lags):
+    def aggie(i, sss):
+        list(map(lambda x: plt.plot(x[0], label=x[1]),zip(sss.w[0:len(lags)], ("t - {}".format(l) for l in lags))))
+        plt.legend()
+        plt.show()
+    return aggie
 
 if __name__ == "__main__":
     # plot_statzi(make_snn_and_run(make_x()), 3)
     # plot_statzi(make_snn_and_run(make_sine()), 3)
     x_plus_sin = make_x(x_scale=5) + make_sine()
-    plot_statzi(make_snn_and_run(x_plus_sin), [2, 3, 5])
+    guy_fawkes = plot_statzi([2, 3, 5])
+    make_snn_and_run(x_plus_sin, guy_fawkes, runs=3)
