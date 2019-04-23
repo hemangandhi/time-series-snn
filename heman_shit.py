03ebc6856b8f60f00e578cbe2b2cd0afdaf88d67
Heman Gandhi <hemangandhi@gmail.com>
2019-04-23 13:31:19 -0400

    it ran

--- heman_shit.py
+++ heman_shit.py
@@ -14,6 +14,7 @@ def make_sine(period_in_dt=21, dt_test=.0001):
 
 def make_snn_and_run_once(ts, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second):
     # constants, equations, detritus
+    start_scope()
     ts = TimedArray(ts, dt=dt_ts)
     N = 1000
     taum = 10*ms
@@ -44,7 +45,7 @@ def make_snn_and_run_once(ts, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * 
     ash = PoissonGroup(1, rates='ts(t)', dt=0.0001 * second)
     lags_ts = TimedArray(lags, dt = 1 * second)
     input = PoissonGroup(len(lags),
-            rates='ts(t - lags_ts(i * second) * {})'.format(dt_ts),
+            rates='ts(t - lags_ts(i * second) * {} * second)'.format(dt_ts),
             dt=0.0001 * second)
     neurons = NeuronGroup(1, eqs_neurons, threshold='v>vt', reset='v = vr',
                         method='euler',dt=0.0001 * second)
@@ -74,7 +75,9 @@ def make_snn_and_run_once(ts, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * 
     # mon = StateMonitor(neurons, variables = ['v'],record=range(10000), dt=0.0001 * second )
 
     # Run and record
-    run(duration, report='text')
+    net = Network(ash, input, neurons, S, S2, sss)
+    net.run(duration, report='text')
+    #TODO: is this a use after free fuxie?
     return sss
 
 def run_many_times(ts, aggregator, runs, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001 * second):
@@ -105,34 +108,49 @@ def lag_is_max(lags):
         lgd[ml] += 1
     return aggie, lgd
 
-def train_and_run(train_data, test_data, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001*second):
-    sss = make_snn_and_run_once(train_data, lats, duration, dt_ts)
+def train_and_run(train_data, test_data, lags=[2, 3, 5], duration=1*second, dt_ts=0.0001*second, test_dur=None):
+    sss = make_snn_and_run_once(train_data, lags, duration, dt_ts)
     lag_to_w = {l: sss.w[:][idx][-1] for idx, l in enumerate(lags)}
     print("Got weights", lag_to_w)
     lag_to_w = [lag_to_w[i] for i in lags]
 
     # brian detrius
+    start_scope()
+    N = 1000
+    taum = 10*ms
+    taue = 5*ms
+    Ee = 0*mV
+    vt = -54*mV
+    vr = -60*mV
+    El = -74*mV
     eqs_neurons = '''
     dv/dt = (ge * (Ee-vr) + El - v) / taum : volt
     dge/dt = -ge / taue : 1
     '''
     lags_ts = TimedArray(lags, dt = 1 * second)
-    w_ts = TimedArray(lag_to_w, dt = 1 * second)
     ts = TimedArray(test_data, dt=dt_ts)
     input = PoissonGroup(len(lags),
-            rates='ts(t - lags_ts(i * second) * {})'.format(dt_ts),
+            rates='ts(t - lags_ts(i * second) * {} * second)'.format(dt_ts),
             dt=0.0001 * second)
     neurons = NeuronGroup(1, eqs_neurons, threshold='v>vt', reset='v = vr',
                         method='euler',dt=0.0001 * second)
     S2 = Synapses(input, neurons,
-                '''w : w_ts(i * second)''',
+                '''w : 1''',
                 on_pre='''ge += w ''',
                 )
+    S2.connect()
+    S2.w = lag_to_w
+
+    if test_dur is None: test_dur = duration
     mon = SpikeMonitor(neurons, variables = ['v'])
-    run(duration, report='text')
+    net = Network(input, neurons, S2, mon)
+    net.run(test_dur, report='text')
+    #TODO: is this too a use after free?
     return mon.values('t')[0]
 
-def rms_error(spikes, observed, rate_est_window=1):
+def rms_error(spikes, observed, rate_est_window=1, dt_ts=0.0001 * second):
+    observed = TimedArray(observed, dt=dt_ts) #easier to force brian to handle the dt bs.
+
     if len(spikes) < rate_est_window:
         return -1
 
@@ -144,7 +162,6 @@ def rms_error(spikes, observed, rate_est_window=1):
 
 if __name__ == "__main__":
     x_plus_sin = make_x(x_scale=5) + make_sine()
-    # guy_fawkes = plot_statzi([2, 3, 5])
-    maxer, di = lag_is_max([2, 3, 5])
-    run_many_times(x_plus_sin, maxer, 300)
-    print(di)
+    spoke = train_and_run(x_plus_sin, x_plus_sin)
+    list(map(print,spoke))
+    print(rms_error(spoke, x_plus_sin))
