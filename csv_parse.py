19c2cc43beecc25593e4701ce7408ac007c3d4ee
Sail338 <srihari.shankar1@gmail.com>
2019-04-23 00:33:17 -0400

    2019 data

--- csv_parse.py
+++ csv_parse.py
@@ -1,4 +1,5 @@
 import csv
+import dateutil.parser
 
 def parseCsvAdj(file):
     out = []
@@ -8,3 +9,15 @@ def parseCsvAdj(file):
             out.append(row['Adj Close'])
     return out
        
+def return2019Data(file):
+    out = []
+    with open(file) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            date = dateutil.parser.parse(row['Date'],ignoretz=True).isoformat()
+            date_split = date.split("-")
+            if(date_split[0] == '2019'):
+                out.append(row['Adj Close'])
+    return out
+
+print(return2019Data("data/AMZN.csv"))
